441681f77d92dd8c08c0d4545cbd26fe07e852fc
Ben_Sheeley <sheeleyb@grinnell.edu>
2024-10-01 14:50:16 -0500

    add basicCounter

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -4,6 +4,8 @@ package edu.grinnell.csc207.experiments;
 // import edu.grinnell.csc207.util.BasicCounter;
 
 import java.io.PrintWriter;
+import edu.grinnell.csc207.util.BasicCounter;
+import edu.grinnell.csc207.util.Counter;
 
 /**
  * Some experiments as we explore our various Counter classes.
@@ -15,9 +17,29 @@ public class CounterExperiments {
    * @param args
    *   The command-line arguments. Ignored.
    */
-  public static void main(String[] args) throws Exception {
+public static void main(String[] args) throws Exception {
+    // Set up output
     PrintWriter pen = new PrintWriter(System.out, true);
 
+    // Set up some counters
+    Counter alpha = new BasicCounter(0);
+    Counter beta = new BasicCounter(123);
+    Counter gamma = new BasicCounter(-5);
+
+    // Print original values
+    pen.println("Original alpha = " + alpha);
+    pen.println("Original beta = " + beta);
+    pen.println("Original gamma = " + gamma);
+  
+    // Print incremented values
+    alpha.increment();
+    beta.increment();
+    gamma.increment();
+    pen.println("Updated alpha = " + alpha);
+    pen.println("Updated beta = " + beta);
+    pen.println("Updated gamma = " + gamma);
+
+    // And we're done
     pen.close();
   } // main(String[])
 } // class CounterExperiments
---
+++ src/main/java/edu/grinnell/csc207/util/BasicCounter.java
@@ -0,0 +1,40 @@
+package edu.grinnell.csc207.util;
+
+import java.lang.String;
+
+public class BasicCounter implements Counter {
+  //fields
+
+  protected int count;
+
+  protected int start;
+
+  //constructro
+
+  public BasicCounter(int val) {
+    this.count = val;
+    this.start = val;
+  }
+
+  //methods
+  public void increment() throws Exception {
+    try {
+      this.count++;
+    }
+    catch(Exception e) {
+      throw new Exception("error");
+    }
+  }
+
+  public void reset() {
+    this.count = this.start;
+  }
+
+  public String toString() {
+    return "[" + this.count + "]";
+  }
+
+  public int get() {
+    return this.count;
+  }
+}
